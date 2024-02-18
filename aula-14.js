efeea904270674416f3389c5566fb9ee88653cb3
BRADDTELES <braddteles@gmail.com>
2024-02-18 12:43:56 -0300

    aula14 Aprendendo sobre String

--- aula-14.js
+++ aula-14.js
@@ -5,7 +5,7 @@ console.log('tamanho: ', mensagem1.length)
 console.log('Concat: ', mensagem1.concat(mensagem2))
 console.log('Concat + ', "Exiba: " + " " + mensagem1 + " " + mensagem2)
 console.log('ES: ',  `Exiba: ${mensagem1} ${mensagem2}`)
-console.log('Quebrar em array: ', mensagem1.split( ' '))
+console.log('Quebrar em array: ', mensagem1.split(' '), mensagem2.split(''))
 
 console.log('UP: ', mensagem1.toUpperCase())
 console.log('DOWN: ', mensagem1.toLowerCase())
