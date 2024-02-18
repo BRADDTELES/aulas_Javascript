215cd642593d36cd47049b232a9ab224f1134ff9
BRADDTELES <braddteles@gmail.com>
2024-02-18 11:05:46 -0300

    Usando a function na aula09

---
+++ aula-09.js
@@ -0,0 +1,11 @@
+
+function soma(item1, item2) {
+    return item1 + item2;
+}
+
+function diminuir(item1, item2){
+    return item1 - item2
+}
+
+console.log('Resultado: ', soma(5, 6));
+console.log('Resultado: ', diminuir(5, 6))
