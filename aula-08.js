19458313624741c6aeb36bbee1adf487a4c15069
BRADDTELES <braddteles@gmail.com>
2024-02-18 10:57:13 -0300

    aula08 Convertendo Number e usando o toFixed

---
+++ aula-08.js
@@ -0,0 +1,6 @@
+const salario = '103'
+const salarioNumero = Number(salario)
+
+console.log('salario',salario)
+console.log('salarioNumero',salarioNumero)
+console.log('salario Numero',salarioNumero.toFixed(2))
