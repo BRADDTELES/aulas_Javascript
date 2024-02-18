958475871c13b7684a25e6ce67a247417435ccb1
BRADDTELES <braddteles@gmail.com>
2024-02-18 10:49:07 -0300

    aula07 Array

---
+++ aula-07.js
@@ -0,0 +1,9 @@
+const idadeJulio = 18
+const idadeJoao = 14;
+const idadeJose = 53;
+
+const idades = [18, 14, 53, 19, 56, 59, 60, 333, 36, 234, 45, 41]
+
+for (let i = 0; i < idades.length; i++) {
+    console.log('Exibir',idades[i]);
+}
